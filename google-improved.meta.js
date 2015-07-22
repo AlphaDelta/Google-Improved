bf3af655706e91d9fb3037db758b1a2ec3a4d0d6
Alfur 'Felt her' Deltur <admin@skid.pro>
2015-07-22 14:51:24 +1200

    Oops!

--- google-improved.meta.js
+++ google-improved.meta.js
@@ -8,7 +8,7 @@
 // @include     https://google.*
 // @version     1.0.3
 // @updateURL   https://raw.githubusercontent.com/AlphaDelta/Google-Improved/master/google-improved.meta.js
-// @downloadURL https://raw.githubusercontent.com/AlphaDelta/Google-Improved/master/google-improved.meta.js
+// @downloadURL https://raw.githubusercontent.com/AlphaDelta/Google-Improved/master/google-improved.user.js
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @icon        https://raw.githubusercontent.com/AlphaDelta/Google-Improved/master/icon.png
